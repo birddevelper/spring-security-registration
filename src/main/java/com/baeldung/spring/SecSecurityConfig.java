6d46380a5f31009062b426cbd455ca3a39a60d0e
birddevelper <mostafa_shaeri_tj@yahoo.com>
2023-03-12 00:49:20 -0800

    Handle unauthorized access with a custom AccessDeniedHandler

--- src/main/java/com/baeldung/spring/SecSecurityConfig.java
+++ src/main/java/com/baeldung/spring/SecSecurityConfig.java
@@ -1,12 +1,15 @@
 package com.baeldung.spring;
 
 import com.baeldung.persistence.dao.UserRepository;
+import com.baeldung.persistence.model.User;
 import com.baeldung.security.CustomRememberMeServices;
 import com.baeldung.security.google2fa.CustomAuthenticationProvider;
 import com.baeldung.security.google2fa.CustomWebAuthenticationDetailsSource;
 import com.baeldung.security.location.DifferentLocationChecker;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,6 +17,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -21,10 +25,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -40,6 +46,8 @@ import java.io.IOException;
 // @ImportResource({ "classpath:webSecurityConfig.xml" })
 @EnableWebSecurity
 public class SecSecurityConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -82,7 +90,7 @@ public class SecSecurityConfig {
                 .antMatchers(HttpMethod.GET, "/roleHierarchy")
                 .hasRole("STAFF")
             .antMatchers("/login*", "/logout*", "/signin/**", "/signup/**", "/customLogin", "/user/registration*", "/registrationConfirm*", "/expiredAccount*", "/registration*", "/badUser*", "/user/resendRegistrationToken*", "/forgetPassword*",
-                "/user/resetPassword*", "/user/savePassword*", "/updatePassword*", "/user/changePassword*", "/emailError*", "/resources/**", "/old/user/registration*", "/successRegister*", "/qrcode*", "/user/enableNewLoc*")
+                "/user/resetPassword*", "/user/savePassword*", "/updatePassword*", "/user/changePassword*", "/emailError*", "/resources/**", "/old/user/registration*", "/successRegister*", "/qrcode*", "/user/enableNewLoc*", "/accessDenied*")
             .permitAll()
             .antMatchers("/invalidSession*")
             .anonymous()
@@ -90,6 +98,10 @@ public class SecSecurityConfig {
             .hasAuthority("CHANGE_PASSWORD_PRIVILEGE")
             .anyRequest()
             .hasAuthority("READ_PRIVILEGE")
+             // Handle unauthorized access with a custom handler
+            .and()
+            .exceptionHandling()
+            .accessDeniedHandler(accessDeniedHandler())
             .and()
             .formLogin()
             .loginPage("/login")
@@ -178,4 +190,19 @@ public class SecSecurityConfig {
     public DifferentLocationChecker differentLocationChecker() {
         return new DifferentLocationChecker();
     }
+
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            // Get the user from request
+            User principal = (User) ((UsernamePasswordAuthenticationToken) request.getUserPrincipal()).getPrincipal();
+            // Display log i console
+            logger.info("User {} attempted to access unauthorized URL {}", principal.getEmail(),request.getRequestURL());
+            // Redirect to accessDenied page
+            response.sendRedirect(request.getContextPath() + "/accessDenied");
+
+        };
+    }
+
 }
