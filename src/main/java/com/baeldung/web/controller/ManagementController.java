e0df5dc8e0122808aeae32bd35322630624c1771
birddevelper <mostafa_shaeri_tj@yahoo.com>
2023-03-12 01:44:56 -0800

    Add ManagementController

---
+++ src/main/java/com/baeldung/web/controller/ManagementController.java
@@ -0,0 +1,19 @@
+package com.baeldung.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Locale;
+
+@Controller
+@RequestMapping("/management")
+public class ManagementController {
+
+    // Management page
+    @GetMapping
+    public String management(final Locale locale) {
+
+        return "private/management";
+    }
+}
